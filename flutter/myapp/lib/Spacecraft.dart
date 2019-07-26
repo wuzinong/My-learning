8403fe44e25f0c93127512cfaed81008e210be58
wuzinong <wuzinong@gmail.com>
2019-07-26 14:22:30 +0800

    update

--- flutter/myapp/lib/Spacecraft.dart
+++ flutter/myapp/lib/Spacecraft.dart
@@ -69,8 +69,7 @@ final aListOfStrings = ['one', 'two', 'three'];
 final aSetOfStrings = {'one', 'two', 'three'};
 final aMapOfStringsToInts = {
   'one': 1,
-  'two': 2,
-  'three': 3,
+  'two': 2
 };
 
 class MyClass{

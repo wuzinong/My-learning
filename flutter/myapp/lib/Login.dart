b5c1a9dcceefcf07be658e7dd07c5d9f0b774d3c
wuzinong <wuzinong@gmail.com>
2019-09-03 16:28:39 +0800

    update style

--- flutter/myapp/lib/Login.dart
+++ flutter/myapp/lib/Login.dart
@@ -10,12 +10,9 @@ class LoginRoute extends StatelessWidget {
         title: Text("用户登录"),
       ),
       body: Center(
-        child: Column(
-            children: <Widget>[
-                    FocusTestRoute()
-                  ],
-        )
-      ),
+          child: Column(
+        children: <Widget>[FocusTestRoute()],
+      )),
     );
   }
 }
@@ -26,106 +23,118 @@ class FocusTestRoute extends StatefulWidget {
 }
 
 class _FocusTestRouteState extends State<FocusTestRoute> {
-  FocusNode focusNode1 = new FocusNode();
-  FocusNode focusNode2 = new FocusNode();
+  // FocusNode focusNode1 = new FocusNode();
+  // FocusNode focusNode2 = new FocusNode();
+  TextEditingController _unameController = new TextEditingController();
+  TextEditingController _pwdController = new TextEditingController();
+  GlobalKey _formKey = new GlobalKey<FormState>();
+  bool _checkboxSelected=false;//维护复选框状态
+
   FocusScopeNode focusScopeNode;
   bool pwdShow = false; //密码是否显示明文
+
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          TextField(
-            // autofocus: true, 
-            focusNode: focusNode1,//关联focusNode1
-            decoration: InputDecoration(
-                labelText: "用户名",
-                hintText: "手机或邮箱",
-                prefixIcon: Icon(Icons.person)
-            ),
-          ),
-          TextField(
-            focusNode: focusNode2,//关联focusNode2
-            decoration: InputDecoration(
-                labelText: "密码",
-                hintText: "您的登录密码",
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: IconButton(
-                      icon: Icon(
-                          pwdShow ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          pwdShow = !pwdShow;
-                        });
-                      },
-                ),
-            ),
-            obscureText: !pwdShow,
-          ),
-        //  TextField(
-        //     keyboardType: TextInputType.emailAddress,
-        //     decoration: InputDecoration(
-        //         labelText: "Email",
-        //         hintText: "电子邮件地址",
-        //         prefixIcon: Icon(Icons.email),
-        //         // border: InputBorder.none //隐藏下划线
-        //     )
-        //   ),
-          Builder(builder: (ctx) {
-            return Column(
-              children: <Widget>[
-                Container(
-                    width: 300.0,
-                    margin:EdgeInsets.only(top:20.0),
-                    child:RaisedButton(
-                    color: Colors.blue,
-                    highlightColor: Colors.blue[700],
-                    colorBrightness: Brightness.dark,
-                    splashColor: Colors.grey,
-                    child: Text("Submit"),
-                    shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Form(
+        key: _formKey, //设置globalKey，用于后面获取FormState
+        autovalidate: true, //开启自动校验
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+                controller: _unameController,
+                decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "手机或邮箱",
+                    prefixIcon: Icon(Icons.person),
+                    errorStyle: TextStyle()),
+                // 校验用户名
+                validator: (v) {
+                  return v.trim().length > 0 ? null : "用户名不能为空";
+                }),
+            TextFormField(
+                controller: _pwdController, //关联focusNode2
+                decoration: InputDecoration(
+                  labelText: "密码",
+                  hintText: "您的登录密码",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon:
+                        Icon(pwdShow ? Icons.visibility_off : Icons.visibility),
                     onPressed: () {
-                          Navigator.push( context,
-                          new MaterialPageRoute(builder: (context) {
-                                  return new WorkStation();
-                          }));
+                      setState(() {
+                        pwdShow = !pwdShow;
+                      });
                     },
-                )
+                  ),
                 ),
-                // RaisedButton.icon(
-                //   icon: Icon(Icons.send),
-                //   label: Text("发送"),
-                //   onPressed: (){},
-                // ),
-                // RaisedButton(
-                //   child: Text("移动焦点"),
-                //   onPressed: () {
-                //     //将焦点从第一个TextField移到第二个TextField
-                //     // 这是一种写法 FocusScope.of(context).requestFocus(focusNode2);
-                //     // 这是第二种写法
-                //     if(null == focusScopeNode){
-                //       focusScopeNode = FocusScope.of(context);
-                //     }
-                //     focusScopeNode.requestFocus(focusNode2);
-                //   },
-                // ),
-                // RaisedButton(
-                //   child: Text("隐藏键盘"),
-                //   onPressed: () {
-                //     // 当所有编辑框都失去焦点时键盘就会收起  
-                //     focusNode1.unfocus();
-                //     focusNode2.unfocus();
-                //   },
-                // ),
+                obscureText: !pwdShow,
+                //校验密码
+                validator: (v) {
+                  return v.trim().length > 5 ? null : "密码不能少于6位";
+                }),
+            //  TextField(
+            //     keyboardType: TextInputType.emailAddress,
+            //     decoration: InputDecoration(
+            //         labelText: "Email",
+            //         hintText: "电子邮件地址",
+            //         prefixIcon: Icon(Icons.email),
+            //         // border: InputBorder.none //隐藏下划线
+            //     )
+            //   ),
+            Row(
+              mainAxisAlignment:MainAxisAlignment.end,
+              children: <Widget>[
+                Checkbox(
+                  value:_checkboxSelected,
+                  activeColor: Colors.blue,
+                  onChanged:(value){
+                      setState(() {
+                        _checkboxSelected=value;
+                      });
+                  }
+                ),
+                Text(
+                  "同意条款"
+                )
               ],
-            );
-          },
-          ),
-        ],
+            ),
+            Builder(
+              builder: (ctx) {
+                return Column(
+                  children: <Widget>[
+                    Container(
+                        width: 300.0,
+                        margin: EdgeInsets.only(top: 20.0),
+                        child: RaisedButton(
+                          color: Colors.blue,
+                          highlightColor: Colors.blue[700],
+                          colorBrightness: Brightness.dark,
+                          splashColor: Colors.grey,
+                          child: Text("Submit"),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          onPressed: () {
+                            if ((_formKey.currentState as FormState).validate() && _checkboxSelected) {
+                              Navigator.push(context,
+                                  new MaterialPageRoute(builder: (context) {
+                                return new WorkStation();
+                              }));
+                            }
+                          },
+                        ))
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
-
 }
--- flutter/myapp/lib/Registration.dart
+++ flutter/myapp/lib/Registration.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import './Login.dart';
 
-
 //Login page
 class RegistrationRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  new FormTestRoute();
+    return new FormTestRoute();
   }
 }
-
 
 class FormTestRoute extends StatefulWidget {
   @override
@@ -20,13 +18,13 @@ class _FormTestRouteState extends State<FormTestRoute> {
   TextEditingController _unameController = new TextEditingController();
   TextEditingController _vCode = new TextEditingController();
   TextEditingController _pwdController = new TextEditingController();
-  GlobalKey _formKey= new GlobalKey<FormState>();
+  GlobalKey _formKey = new GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("用户注册"),
+        title: Text("用户注册"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
@@ -40,55 +38,74 @@ class _FormTestRouteState extends State<FormTestRoute> {
                   decoration: InputDecoration(
                       labelText: "用户名",
                       hintText: "手机或邮箱",
-                      icon: Icon(Icons.person)
-                  ),
+                      icon: Icon(Icons.person)),
                   // 校验用户名
                   validator: (v) {
-                    return v
-                        .trim()
-                        .length > 0 ? null : "用户名不能为空";
-                  }
-
-              ),
+                    return v.trim().length > 0 ? null : "用户名不能为空";
+                  }),
               TextFormField(
                   controller: _vCode,
                   decoration: InputDecoration(
                       labelText: "验证码",
                       hintText: "请输入验证码",
-                      icon: Icon(Icons.near_me)
-                  ),
+                      icon: Icon(Icons.near_me)),
                   // 校验用户名
                   validator: (v) {
-                    return v
-                        .trim()
-                        .length > 0 ? null : "验证码不能为空";
-                  }
-
-              ),
-              Text(
-                "获取验证码",
-                textAlign:TextAlign.left,
-                style:TextStyle(
-                  color:Colors.lightBlue,
-                  fontSize:16,
-                  decoration: TextDecoration.underline,
-                )
+                    return v.trim().length > 0 ? null : "验证码不能为空";
+                  }),
+              Row(
+                children: <Widget>[
+                  FlatButton(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Text("获取验证码",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,
+                        )),
+                    onPressed: () {
+                      showDialog<Null>(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return new AlertDialog(
+                            title: new Text('验证码信息:'),
+                            content: new SingleChildScrollView(
+                              child: new ListBody(
+                                children: <Widget>[
+                                  new Text('您的验证码已发送，请注意查收'),
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              new FlatButton(
+                                child: new Text('确定'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      ).then((val) {
+                        print(val);
+                      });
+                    },
+                  )
+                ],
               ),
               TextFormField(
                   controller: _pwdController,
                   decoration: InputDecoration(
                       labelText: "密码",
                       hintText: "您的登录密码",
-                      icon: Icon(Icons.lock)
-                  ),
+                      icon: Icon(Icons.lock)),
                   obscureText: true,
                   //校验密码
                   validator: (v) {
-                    return v
-                        .trim()
-                        .length > 5 ? null : "密码不能少于6位";
-                  }
-              ),
+                    return v.trim().length > 5 ? null : "密码不能少于6位";
+                  }),
               // 登录按钮
               Padding(
                 padding: const EdgeInsets.only(top: 28.0),
@@ -98,9 +115,7 @@ class _FormTestRouteState extends State<FormTestRoute> {
                       child: RaisedButton(
                         padding: EdgeInsets.all(15.0),
                         child: Text("注册"),
-                        color: Theme
-                            .of(context)
-                            .primaryColor,
+                        color: Theme.of(context).primaryColor,
                         textColor: Colors.white,
                         onPressed: () {
                           //在这里不能通过此方式获取FormState，context不对
@@ -108,14 +123,14 @@ class _FormTestRouteState extends State<FormTestRoute> {
 
                           // 通过_formKey.currentState 获取FormState后，
                           // 调用validate()方法校验用户名密码是否合法，校验
-                          // 通过后再提交数据。 
-                          if((_formKey.currentState as FormState).validate()){
+                          // 通过后再提交数据。
+                          if ((_formKey.currentState as FormState).validate()) {
                             //验证通过提交数据
-                                //导航到新路由   
-                              Navigator.push( context,
-                              new MaterialPageRoute(builder: (context) {
-                                      return new LoginRoute();
-                              }));
+                            //导航到新路由
+                            Navigator.push(context,
+                                new MaterialPageRoute(builder: (context) {
+                              return new LoginRoute();
+                            }));
                           }
                         },
                       ),

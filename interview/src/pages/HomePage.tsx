2efe92c4b63bac962609f56d7c10be53c5940f75
Gu <Xie.Hai.Bran.Gu@dnv.com>
2023-02-01 10:52:57 +0800

    update question

--- interview/src/pages/HomePage.tsx
+++ interview/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -17,7 +17,11 @@ const HomePage: FC = () => {
   // TODO
   // 1. Add a header and footer for this website
   // 2. Calling /api/users to get users
-  // 3. Show the user list below
+  // 3. Show the user list in table:
+  //    table head: Name, Gender, Action
+  //    looping the user list to the table and add 1 delete button in Action column
+  //    when user click the row will dierect him to detail page
+  //    when user click delete button, show some message in console
   // 4. Create a user details page which shows the user name and gender (/api/user?id=xxx)
   // 5. Add Link to the user list which would direct user to the user details page
 

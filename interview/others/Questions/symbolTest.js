a7cdcc8d90a694dd51e59ad9fd0d7ed0c35951c7
Gu <Xie.Hai.Bran.Gu@dnv.com>
2023-07-19 12:47:33 +0800

    update

--- interview/others/Questions/symbolTest.js
+++ interview/others/Questions/symbolTest.js
@@ -3,3 +3,26 @@ Object.prototype[Symbol.iterator] = function () {
 };
 
 const [a, b] = { a: 1, b: 2 };
+
+//对obj改造
+let obj = {
+  data: ['name:test', 'age:18', 'sex:男'],
+  [Symbol.iterator]: function () {
+    const self = this;
+    let index = 0;
+    return {
+      next: function () {
+        if (index < self.data.length) {
+          return {
+            value: self.data[index++],
+            done: false,
+          };
+        }
+        return { value: undefined, done: true };
+      },
+    };
+  },
+};
+for (let i of obj) {
+  console.log(i);
+}
